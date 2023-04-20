7eb7ea3002d9981bcf3f5c2e7ba440a85713104a
teaua <131398427+teaua@users.noreply.github.com>
2023-04-21 00:28:17 +0300

    Update OsagoInfoForm.vue

--- OsagoInfoForm.vue
+++ OsagoInfoForm.vue
@@ -1,27 +1,59 @@
 <script>
+import analyticsService from '@/services/analytics-service';
 
-    import analyticsService from '@/services/analytics-service';
+export default {
+  data: function () {
+    return {
+      // ...
+    };
+  },
+  methods: {
+    onPolicySelected() {
+      const policyData = {
+        amount: this.policyAmount,
+        currency: this.policyCurrency,
+        id: this.policyId,
+        type: this.policyType
+      };
+      analyticsService.sendPolicySelected(policyData);
+    },
 
-    export default {
-    
-        data: function () {
-            return {
-              
-            };
-        },
-        mounted: function () {
-            const policyData = { amount: 1000, currency: "UAH", id: 1232, type: "moto" };
-            analyticsService.sendPolicySelected(policyData);
-         },
+    onContactInfoEntered() {
+      const contactData = this.contactData;
+      analyticsService.sendContactInfoEntered(contactData);
+    },
 
-        computed: {
-          },
+    onCarDataEntered() {
+      const carData = this.carData;
+      analyticsService.sendCarDataEntered(carData);
+    },
 
-        watch: {
-           
-        },
+    onDocumentsEntered() {
+      const documentData = this.documentData;
+      analyticsService.sendDocumentsEntered(documentData);
+    },
 
-        methods: {
-        }
-    }
+    onDgoSelected() {
+      const dgoData = this.dgoData;
+      analyticsService.sendDgoSelected(dgoData);
+    },
+
+    onContractCreated() {
+      const contractData = this.contractData;
+      analyticsService.sendContractCreated(contractData);
+    },
+
+    onPurchase() {
+      const amount = this.purchaseAmount;
+      const currency = this.purchaseCurrency;
+      const transactionId = this.transactionId; // динамический идентификатор транзакции
+      const itemId = this.itemId; // динамический идентификатор товара
+      const itemName = this.itemName; // динамическое имя товара
+
+      analyticsService.sendPurchase(amount, currency, transactionId, itemId, itemName);
+    },
+  },
+};
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
