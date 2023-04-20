858b43478df7a179a4d06844900c02544c48ec8e
teaua <131398427+teaua@users.noreply.github.com>
2023-04-20 23:45:38 +0300

    Update analytics-service.js

--- analytics-service.js
+++ analytics-service.js
@@ -9,58 +9,62 @@ Vue.use(VueGtag, {
 
 const analyticsService = {
     sendPolicySelected(policyData) {
-        Vue.$gtag.event("policy_selected", {
-            value: policyData.amount,
-            currency: policyData.currency,
-            policy_id: policyData.id,
-            policy_type: policyData.type,
+        Vue.$gtag.event("select_item", {
+            items: [
+                {
+                    item_id: policyData.id,
+                    item_name: policyData.type,
+                    item_category: "policy",
+                    price: policyData.amount,
+                    currency: policyData.currency,
+                },
+            ],
         });
     },
-
-    sendContactInfoEntered(contactData) {
-        Vue.$gtag.event("contact_info_entered", {
-            name: contactData.name,
-            surname: contactData.surname,
-            phone: contactData.phone,
-            email: contactData.email,
-        });
-    },
-
-    sendCarDataEntered(carData) {
-        Vue.$gtag.event("car_data_entered", {
-            make: carData.make,
-            model: carData.model,
-            year: carData.year,
-            registration_number: carData.registration_number,
-        });
-    },
-
-    sendDocumentsEntered(documentData) {
-        Vue.$gtag.event("documents_entered", {
-            personal_id: documentData.personal_id,
-            passport_data: documentData.passport_data,
-            address: documentData.address,
-        });
-    },
-
     sendDgoSelected(dgoData) {
         Vue.$gtag.event("dgo_selected", {
-            dgo_value: dgoData.dgo_value,
-            dgo_currency: dgoData.dgo_currency,
+            dgo_id: dgoData.id,
+            dgo_name: dgoData.name,
         });
     },
-
+    sendContactInfoEntered() {
+        Vue.$gtag.event("begin_checkout", {
+            checkout_step: 1,
+            step_label: "contact_info_entered",
+        });
+    },
+    sendCarDataEntered() {
+        Vue.$gtag.event("begin_checkout", {
+            checkout_step: 2,
+            step_label: "car_data_entered",
+        });
+    },
+    sendDocumentsEntered() {
+        Vue.$gtag.event("begin_checkout", {
+            checkout_step: 3,
+            step_label: "documents_entered",
+        });
+    },
     sendContractCreated(contractData) {
         Vue.$gtag.event("contract_created", {
-            contract_id: contractData.contract_id,
-            contract_date: contractData.contract_date,
+            contract_id: contractData.id,
+            contract_type: contractData.type,
         });
     },
-
-    sendPurchase(amount, currency) {
+    sendPaymentSuccess(purchaseData) {
         Vue.$gtag.event("purchase", {
-            value: amount,
-            currency: currency,
+            transaction_id: purchaseData.transaction_id,
+            value: purchaseData.amount,
+            currency: purchaseData.currency,
+            items: [
+                {
+                    item_id: purchaseData.policy_id,
+                    item_name: purchaseData.policy_type,
+                    item_category: "policy",
+                    price: purchaseData.amount,
+                    currency: purchaseData.currency,
+                },
+            ],
         });
     },
 };
