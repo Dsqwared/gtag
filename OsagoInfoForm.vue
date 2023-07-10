39bdf31013e70c8d01bea61692fcd57fbd445f12
dsqwared5 <105047585+dsqwared5@users.noreply.github.com>
2023-07-10 13:56:56 +0300

    Update OsagoInfoForm.vue

--- OsagoInfoForm.vue
+++ OsagoInfoForm.vue
@@ -1,67 +1,228 @@
 <script>
-import analyticsService from '@/services/analytics-service';
-export default {
-  data: function () {
+    import analyticsService from '@/services/analytics-service';
+    export default {
+    data: function () {
     return {
-      // ...
-    };
-  },
-  methods: {
-    onClose() {
-      analyticsService.sendClosedPopUp();
-    },
-    onShow() {
-      analyticsService.sendOpenedPopUp();
-    },
-    onSendPopupdata() {
-      let data = {
-        phone: this.phone,
-        name: this.name,
-        end_insurance: this.dateEnd,
-        type_id: this.insuranceType,
-      };
-      analyticsService.sendSendPopUpData(data);
-    },
+    // ...
+};
+},
+    methods: {
+    sendClosedPopUp() {
+    analyticsService.sendClosedPopUp();
+},
+    sendOpenedPopUp() {
+    analyticsService.sendOpenedPopUp();
+},
+    sendSendPopUpData() {
+    let data = {
+    phone: this.phone,
+    name: this.name,
+    end_insurance: this.dateEnd,
+    type_id: this.insuranceType
+};
+    analyticsService.sendSendPopUpData(data);
+},
     onSendHomePageView() {
-      analyticsService.sendHomePageView();
-    },
+    analyticsService.sendHomePageView();
+},
+    sendOsagoPageView() {
+    analyticsService.sendHomePageView();
+},
     onSendOsagoOrderCarEntered() {
-      let params = {
-        plate: plate,
-        year: this.year,
-        vin: this.vin,
-        model: this.model,
-        mark: this.mark,
-      };
-      analyticsService.sendOsagoOrderCarEntered(params);
-    },
+    let params = {
+    plate: plate,
+    year: this.year,
+    vin: this.vin,
+    model: this.model,
+    mark: this.mark,
+    value:finalPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    item_brand: "Ukasko",
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    price: finalPrice,
+    quantity: 1, // количество товара
+}
+    ]
+};
+    analyticsService.sendOsagoOrderCarEntered(params);
+},
+    sendOsagoBeginCheckout() {
+    let params = {order_id:this.orderId,
+    value:orderPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    item_brand: "Ukasko",
+    price: orderPrice,
+    quantity: 1, // количество товара
+}
+    ]};
+    analyticsService.sendOsagoBeginCheckout(params);
+},
     onSendOsagoOrderCheckout() {
-      analyticsService.sendOsagoOrderCheckout(this.orderId);
-    },
+    let params = {order_id:this.orderId,
+    value:orderPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    item_brand: "Ukasko",
+    price: orderPrice,
+    quantity: 1, // количество товара
+}
+    ]};
+    analyticsService.sendOsagoOrderCheckout(params);
+},
     onSendOsagoSetPromo() {
-      let params = { order_id: this.orderId, promoCode: this.selectedPromo };
-      analyticsService.sendOsagoSetPromo(params);
-    },
+    let params = {order_id: this.orderId, promoCode:null};
+    analyticsService.sendOsagoSetPromo(params);
+},
     onSendOsagoOrderAdditionalCoverSelected() {
-      analyticsService.sendOsagoOrderAdditionalCoverSelected(this.dataDgo);
-    },
+    let params = {
+    dgo_data: this.selectedDgo,
+    value:finalPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    item_brand: "Ukasko",
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    price: finalPrice,
+    quantity: 1, // количество товара
+}
+    ]
+};
+
+    analyticsService.sendOsagoOrderAdditionalCoverSelected(params);
+},
     onSendOsagoOrderDocumentEntered() {
-      analyticsService.sendOsagoOrderDocumentEntered(params);
-    },
+    let params = {
+    documentType: this.documentType,
+    documentDate: this.documentDate,
+    documentIssued: this.documentIssued,
+    documentSeries: this.documentSeries,
+    documentNumber: this.documentNumber,
+    tax_number: this.tax_number,
+    address: this.address,
+    birth_date: this.birth_date,
+    dgo:this.dataDgo,
+    value:finalPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    item_brand: "Ukasko",
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    price: finalPrice,
+    quantity: 1, // количество товара
+}
+    ]
+};
+    analyticsService.sendOsagoOrderDocumentEntered(params);
+},
     onSendOsagoPurchase() {
-      analyticsService.sendOsagoPurchase(params);
-    },
+    let params = {transaction_id: this.order.id, value:finalPrice, currency:"UAH", coupon:this.promo_code,
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    item_brand: "Ukasko",
+    price: etalon_sum,
+    discount: discont_sum,
+    coupon:this.promo_code,
+    franchise:this.order.tariff.franchise,
+    company_name:this.order.tariff.company.name,
+    quantity: 1, // количество товара
+}
+    ]};
+
+    analyticsService.sendOsagoPurchase(params);
+},
     onSendOsagoOrderPersonalEntered() {
-      analyticsService.sendOsagoOrderPersonalEntered(params);
-    },
-    onSendEventOsagoSelect() {
-      analyticsService.sendOsagoSelect();
-    },
+    let params = {
+    user_name: this.user_name,
+    phone: this.phone,
+    email: this.email,
+    dgo:this.dataDgo,
+    value:finalPrice,
+    currency:"UAH",
+    items: [
+{
+    item_id: this.order.tariff.id,
+    item_name: this.order.tariff.slug,
+    item_brand: "Ukasko",
+    price: finalPrice,
+    quantity: 1, // количество товара
+}
+    ]
+};
+    analyticsService.sendOsagoOrderPersonalEntered(params);
+},
+    sendOsagoSelect() {
+    analyticsService.sendOsagoSelect();
+},
     onSendOsagoPolicySelected() {
-      const policyData = { id: item.id, franchise: item.franchise, company: item.company.name };
-      analyticsService.sendOsagoPolicySelected(policyData);
-    },
-  },
+    const policyData = {value:finalPrice, currency:"UAH",
+    items: [
+{
+    item_id: item.id,
+    item_name: lastSegment,
+    item_brand: "Ukasko",
+    price: finalPrice,
+    item_franchise:item.franchise,
+    company:item.company.name,
+    quantity: 1, // количество товара
+}
+    ]}
+    analyticsService.sendOsagoPolicySelected(policyData);
+},
+    sendOsagoPolicyAddToCart() {
+    const policyData = {value:finalPrice, currency:"UAH",
+    items: [
+{
+    item_id: item.id,
+    item_name: lastSegment,
+    item_brand: "Ukasko",
+    price: finalPrice,
+    item_franchise:item.franchise,
+    company:item.company.name,
+    quantity: 1, // количество товара
+}
+    ]}
+    analyticsService.sendOsagoPolicyAddToCart(policyData);
+},
+    sendOsagoViewItem() {
+    const policyData = {value:finalPrice, currency:"UAH",
+    items: [
+{
+    item_id: item.id,
+    item_name: lastSegment,
+    item_brand: "Ukasko",
+    price: finalPrice,
+    item_franchise:item.franchise,
+    company:item.company.name,
+    quantity: 1, // количество товара
+}
+    ]}
+    analyticsService.sendOsagoViewItem(policyData);
+},
+},
 };
 </script>
 
